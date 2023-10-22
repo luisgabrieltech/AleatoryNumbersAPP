d0ad76ea2eb3893d34be50f4fcbf7200e89749cb
luisgabrieltech <luisgabrieltech@gmail.com>
2023-10-22 02:17:25 -0300

    Att: Add Notificações

--- app/src/main/java/com/luisgabriel/aleatorynumbers/MainActivity.kt
+++ app/src/main/java/com/luisgabriel/aleatorynumbers/MainActivity.kt
@@ -3,6 +3,7 @@ package com.luisgabriel.aleatorynumbers
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
@@ -20,13 +21,8 @@ class MainActivity : AppCompatActivity() {
             val numeroAleatorio = Random.nextInt(21)
             resultadogeradornumero.text = "Numero gerado: $numeroAleatorio"
 
-        //val mensagemGerador = Toast.makeText( this, "O Número foi gerado!", Toast.LENGTH_LONG )
-        //mensagemGerador.show()
+        val mensagemGerador = Toast.makeText( this, "O Número foi gerado!", Toast.LENGTH_LONG )
+        mensagemGerador.show()
         }
     }
-
-    class Dice () {
-
-    }
-
 }
